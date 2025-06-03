f4d69257bf1a8b9c6afa044a46b327627e1b894c
saraiGtz <sgutierrez@videsa.com.mx>
2025-06-03 16:48:55 -0600

    integracion de formulario para Norma 85 hecho por Raul

---
+++ application/controllers/normas/Nom_085.php
@@ -0,0 +1,32 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	class Nom_085 extends CI_Controller{
+		function __construct(){
+			parent::__construct();
+
+            			if(!$this->session->userdata('logged_in')){
+                        				redirect('login', 'refresh');
+
+                        }
+
+        }
+		function index(){
+				$data_session = $this->session->userdata('logged_in');
+				$usuario_nivel = $data_session['id_usuario_nivel'];
+				
+				$data['nombre_completo'] = $data_session['nombre_completo'];
+				$data['tienda'] = $data_session['tienda'];
+				$data_menus['menus'] = $data_session['menus'];
+				
+				$this->load->view('template/header', $data_menus);
+				$this->load->view('normas/085', $data);
+				$this->load->view('template/footer');
+
+		}
+	
+
+
+		
+	}	
+		
+?>
---
+++ application/views/normas/085.php
@@ -0,0 +1,381 @@
+ 	<!-- Main Container -->
+            <main id="main-container">
+                <div class="content"> <!-- Page Content -->
+                  
+<!--******************************CONTENT-->
+
+<h1>Captura de Norma 085</h1>
+<hr>
+
+
+
+<div class="row">
+
+
+<div class="col-md-12">
+
+
+
+
+<div id="div1">
+
+      <form>
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Número de Informe:</label>
+            <input type="text" class="form-control" value="FE085MG/250405-01">
+          </div>
+          <div class="col-md-4">
+            <label>Orden de Servicio:</label>
+            <input type="text" class="form-control" value="25-1347">
+          </div>
+          <div class="col-md-4">
+            <label>Fecha de Evaluación:</label>
+            <input type="date" class="form-control" value="2025-04-13">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-6">
+            <label>Recepción:</label>
+            <input type="date" class="form-control" value="2025-04-06">
+          </div>
+          <div class="col-md-6">
+            <label>Fecha de Informe:</label>
+            <input type="date" class="form-control" value="2025-04-13">
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="row mb-3">
+          <div class="col-md-6">
+            <label>Razón Social:</label>
+            <input type="text" class="form-control" value="Flexico, S. de R.L. de C.V.">
+          </div>
+          <div class="col-md-6">
+            <label>Calle y Número:</label>
+            <input type="text" class="form-control" value="Carretera Jilotepec -Soyaniquilpan Km 3.5, Mz. 2 Lt. 1B">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Colonia:</label>
+            <input type="text" class="form-control" value="Parque Industrial Jilotepec">
+          </div>
+          <div class="col-md-4">
+            <label>Alcaldía o Municipio:</label>
+            <input type="text" class="form-control" value="Jilotepec">
+          </div>
+          <div class="col-md-4">
+            <label>Estado:</label>
+            <input type="text" class="form-control" value="Estado de México">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Código Postal:</label>
+            <input type="text" class="form-control" value="54240">
+          </div>
+          <div class="col-md-4">
+            <label>Responsable:</label>
+            <input type="text" class="form-control" value="Juan Fernando Romero Martínez">
+          </div>
+          <div class="col-md-4">
+            <label>Cargo:</label>
+            <input type="text" class="form-control" value="Jefe de HSE">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Teléfono:</label>
+            <input type="text" class="form-control" value="55 55 80 80 00">
+          </div>
+        </div>
+      </form>
+<hr>
+
+</div>
+<div id="div2">
+
+      <form>
+        <div class="row mb-3">
+          <div class="col-md-6">
+            <label>Equipo evaluado:</label>
+            <input type="text" class="form-control" value="Plancha 3">
+          </div>
+          <div class="col-md-6">
+            <label>Marca y Modelo:</label>
+            <input type="text" class="form-control" value="No disponible">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-6">
+            <label>Combustible Utilizado:</label>
+            <input type="text" class="form-control" value="Gas Natural">
+          </div>
+          <div class="col-md-6">
+            <label>Capacidad térmica nominal:</label>
+            <input type="text" class="form-control" >
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Altura msnm:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Presión estática:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Año:</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Presión Barométrica:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Geometría del conducto:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Diámetro interior del conducto, Dch:</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Diámetro equivalente, Deq:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Largo transversal, L1:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Ancho transversal, L2:</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Número de puertos:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Distancia en A:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Distancia en B:</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Distancia en C:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Extensión del puerto, epm:</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <label>Número de diámetros en A:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Número de diámetros en B:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-md-4">
+            <label>Número de diámetros en C:</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-6">
+            <label>Número de puntos seleccionados para medición de gases:</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+      </form>
+
+
+
+</div>
+
+<div id="div3">
+<h2>Determinación de la estratificación</h2>
+<table class="table" border="1" cellpadding="5" cellspacing="0">
+  <caption><strong>Analito: NOx</strong></caption>
+  <thead>
+    <tr>
+      <th>Marcado de la sonda (m)</th>
+      <th>Concentración (ppm o %vol.)</th>
+      <th>% Estratificación</th>
+      <th>ppm</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><input type="number" step="0.001" value="0.03"></td>
+      <td><input type="number" step="0.01" value="8.1"></td>
+      <td><input type="number" step="0.01" value="4.33"></td>
+      <td><input type="number" step="0.01" value="0.37"></td>
+    </tr>
+    <tr>
+      <td><input type="number" step="0.001" value="0.075"></td>
+      <td><input type="number" step="0.01" value="8.4"></td>
+      <td><input type="number" step="0.01" value="0.79"></td>
+      <td><input type="number" step="0.01" value="0.07"></td>
+    </tr>
+    <tr>
+      <td><input type="number" step="0.001" value="0.13"></td>
+      <td><input type="number" step="0.01" value="8.9"></td>
+      <td><input type="number" step="0.01" value="5.12"></td>
+      <td><input type="number" step="0.01" value="0.43"></td>
+    </tr>
+    <tr>
+      <td><strong>Promedio</strong></td>
+      <td><input type="number" step="0.01" value="8.47"></td>
+      <td><strong>Máximo</strong></td>
+      <td>
+        <input type="number" step="0.01" value="0.43">
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+</div>
+
+<div id="div4">
+
+
+<h2>Captura de datos de campo</h2>
+
+<table class="table" border="1" cellpadding="1" cellspacing="0">
+  <caption><strong>Captura de datos de campo</strong></caption>
+  <thead>
+    <tr>
+      <th>No.</th>
+      <th>Nox (ppmv)</th>
+      <th>CO (ppmv)</th>
+      <th>O2 (%)</th>
+      <th>CO2 (%)</th>
+      <th>TEMP. En el conducto (°C)</th>
+    </tr>
+  </thead>
+  <tbody>
+     <tr><td>1</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+     <tr><td>2</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+     <tr><td>3</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+     <tr><td>4</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+     <tr><td>5</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+     <tr><td>6</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+     <tr><td>7</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+     <tr><td>8</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+     <tr><td>9</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>10</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>11</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>12</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>13</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>14</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>15</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>16</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>17</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>18</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>19</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>20</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>21</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>22</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>23</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>24</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>25</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>26</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>27</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>28</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>29</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>30</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>31</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>32</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>33</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>34</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>35</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>36</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>37</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>38</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>39</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>40</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>41</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>42</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>43</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>44</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>45</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>46</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>47</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>48</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>49</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>50</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>51</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>52</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>53</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>54</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>55</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>56</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>57</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>58</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>59</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+    <tr><td>60</td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td></tr>
+</tbody>
+</table>
+
+<br><br><br> <hr>
+
+
+</div>
+
+</div>
+
+
+
+</div>
+
+
+
+
+
+<!--******************************CONTENT-->
+                </div> <!-- END Page Content -->
+            </main>
+            <!-- END Main Container -->
+            <script type="text/javascript">
+				
+            	$(document).ready(function(){
+                
+                    document.title = 'NOM 085'; // titulo de la pag
+
+                    dashboardAdmin();
+                    
+                });
+            
+            </script>
