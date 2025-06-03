28d2ac5f053238d06c175352fde33fafd203cb32
FreddieVen <fventura@videsa.com.mx>
2025-06-03 16:38:04 -0600

    Corecciones JavaScript

--- assets/js/norma85/forms/formulario2.js
+++ assets/js/norma85/forms/formulario2.js
@@ -37,17 +37,54 @@ $(document).ready(function(){
         </form>
     `;
 
-    $("#normaSelect").change(function(){
-        let opcion = $("#normaSelect").val();
-        if(opcion == "085MG" || opcion == "085ML"){
-            $('#tablas').html(tabla);
-            filas();
-        }else{
-            $('#tablas').empty();
-        }
-    });
 
-    
+    let tabla2 = `
+        <form action="">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr class="text-center">
+                            <th>N°</th>
+                            <th>CO (ppmv)</th>
+                            <th>O2(%)</th>
+                            <th>CO<sub>2</sub> %</th>
+                            <th>Temp, En el Conducto C°</th>
+                        </tr>
+                    </thead>
+                    <tbody id="CamposRegistros2">
+                        
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="4"></td>
+                            <td colspan="1">
+                                <div class="d-grid">
+                                    <input type="submit" value="Agregar" class="btn btn-primary">
+                                </div>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </form>
+    `;
+
+    function filas2(){
+        $("#CamposRegistros2").empty();
+
+        for(let i=0;i<60; i++){
+            let campo = ` 
+                <tr> 
+                    <td>${i+1}</td> 
+                    <td><input type="number" class="form-control" name="CO"/></td> 
+                    <td><input type="number" class="form-control" name="O2" step="0.01"/></td> 
+                    <td><input type="number" class="form-control" name="CO2" step="0.01"/></td> 
+                    <td><input type="number" class="form-control" name="Temp" step="0.1"/></td> 
+                </tr>`; 
+
+            $("#CamposRegistros2").append(campo);
+        }
+    }
 
     function filas(){
         $("#CamposRegistros").empty();
@@ -66,5 +103,16 @@ $(document).ready(function(){
             $("#CamposRegistros").append(campo);
         }
     }
+
+    $("#normaSelect").change(function(){
+        let opcion = $("#normaSelect").val();
+        if(opcion == "085MG" || opcion == "085ML"){
+            $('#tablas').html(tabla);
+            filas();
+        }else if(opcion == "085G" || opcion == "085L"){
+            $('#tablas').html(tabla2);
+            filas2();
+        }
+    });
     
 });
