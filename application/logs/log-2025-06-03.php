01ea0ae6e64479e7396c37d866930a545700b839
saraiGtz <sgutierrez@videsa.com.mx>
2025-06-03 11:52:43 -0600

    Correciones en rama

--- application/logs/log-2025-06-03.php
+++ application/logs/log-2025-06-03.php
@@ -23,3 +23,4 @@ ERROR - 2025-06-03 18:57:07 --> 404 Page Not Found: Assets/img
 ERROR - 2025-06-03 18:57:16 --> 404 Page Not Found: Assets/img
 ERROR - 2025-06-03 19:33:23 --> Severity: error --> Exception: Unable to locate the model you have specified: Norma85Model C:\xampp2\htdocs\videsa_app\system\core\Loader.php 348
 ERROR - 2025-06-03 19:35:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-06-03 19:51:52 --> 404 Page Not Found: Assets/js
