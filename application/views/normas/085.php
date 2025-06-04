b32c1c6ae0c2e18483aebbfa6da974f8dfae43b6
saraiGtz <sgutierrez@videsa.com.mx>
2025-06-04 08:31:43 -0600

    se integra variables a pdf

--- application/views/normas/085.php
+++ application/views/normas/085.php
@@ -1,178 +1,150 @@
- 	<!-- Main Container -->
-            <main id="main-container">
-                <div class="content"> <!-- Page Content -->
-                  
-<!--******************************CONTENT-->
-
+ <main id="main-container">
+ <div class="content"> 
 <h1>Captura de Norma 085</h1>
 <hr>
-
-
-
 <div class="row">
-
-
 <div class="col-md-12">
-
-
-
-
 <div id="div1">
-
       <form>
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Número de Informe:</label>
-            <input type="text" class="form-control" value="FE085MG/250405-01">
+            <input type="text" class="form-control"  name="numero_informe" value="FE085MG/250405-01">
           </div>
           <div class="col-md-4">
             <label>Orden de Servicio:</label>
-            <input type="text" class="form-control" value="25-1347">
+            <input type="text" class="form-control"  name="orden_servicio" value="25-1347">
           </div>
           <div class="col-md-4">
             <label>Fecha de Evaluación:</label>
-            <input type="date" class="form-control" value="2025-04-13">
+            <input type="date" class="form-control" name="fecha_evaluacion" value="2025-04-13">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-6">
             <label>Recepción:</label>
-            <input type="date" class="form-control" value="2025-04-06">
+            <input type="date" class="form-control" name="recepcion" value="2025-04-06">
           </div>
           <div class="col-md-6">
             <label>Fecha de Informe:</label>
-            <input type="date" class="form-control" value="2025-04-13">
+            <input type="date" class="form-control" name="fecha_informe" value="2025-04-13">
           </div>
         </div>
-
-        <hr>
-
         <div class="row mb-3">
           <div class="col-md-6">
             <label>Razón Social:</label>
-            <input type="text" class="form-control" value="Flexico, S. de R.L. de C.V.">
+            <input type="text" class="form-control" name="razon_social" value="Flexico, S. de R.L. de C.V.">
           </div>
           <div class="col-md-6">
             <label>Calle y Número:</label>
-            <input type="text" class="form-control" value="Carretera Jilotepec -Soyaniquilpan Km 3.5, Mz. 2 Lt. 1B">
+            <input type="text" class="form-control" name="calle" value="Carretera Jilotepec -Soyaniquilpan Km 3.5, Mz. 2 Lt. 1B">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Colonia:</label>
-            <input type="text" class="form-control" value="Parque Industrial Jilotepec">
+            <input type="text" class="form-control" name="colonia" value="Parque Industrial Jilotepec">
           </div>
           <div class="col-md-4">
             <label>Alcaldía o Municipio:</label>
-            <input type="text" class="form-control" value="Jilotepec">
+            <input type="text" class="form-control" name="alcaldia" value="Jilotepec">
           </div>
           <div class="col-md-4">
             <label>Estado:</label>
-            <input type="text" class="form-control" value="Estado de México">
+            <input type="text" class="form-control" name="estado" value="Estado de México">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Código Postal:</label>
-            <input type="text" class="form-control" value="54240">
+            <input type="text" class="form-control" name="cp"  value="54240">
           </div>
           <div class="col-md-4">
             <label>Responsable:</label>
-            <input type="text" class="form-control" value="Juan Fernando Romero Martínez">
+            <input type="text" class="form-control" name="responsable" value="Juan Fernando Romero Martínez">
           </div>
           <div class="col-md-4">
             <label>Cargo:</label>
-            <input type="text" class="form-control" value="Jefe de HSE">
+            <input type="text" class="form-control" name="cargo" value="Jefe de HSE">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Teléfono:</label>
-            <input type="text" class="form-control" value="55 55 80 80 00">
+            <input type="text" class="form-control" name="telefono" value="55 55 80 80 00">
           </div>
         </div>
       </form>
-<hr>
-
 </div>
 <div id="div2">
-
       <form>
         <div class="row mb-3">
           <div class="col-md-6">
             <label>Equipo evaluado:</label>
-            <input type="text" class="form-control" value="Plancha 3">
+            <input type="text" class="form-control" name="equipo_evaluado" value="Plancha 3">
           </div>
           <div class="col-md-6">
             <label>Marca y Modelo:</label>
-            <input type="text" class="form-control" value="No disponible">
+            <input type="text" class="form-control" name="marca" value="No disponible">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-6">
             <label>Combustible Utilizado:</label>
-            <input type="text" class="form-control" value="Gas Natural">
+            <input type="text" class="form-control" name="combustible" value="Gas Natural">
           </div>
           <div class="col-md-6">
             <label>Capacidad térmica nominal:</label>
-            <input type="text" class="form-control" >
+            <input type="text" name="capacidad_termica" class="form-control" >
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Altura msnm:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="altura" class="form-control">
           </div>
           <div class="col-md-4">
             <label>Presión estática:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="presion" class="form-control">
           </div>
           <div class="col-md-4">
             <label>Año:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="anio" class="form-control">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Presión Barométrica:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="presion_barometrica" class="form-control">
           </div>
           <div class="col-md-4">
             <label>Geometría del conducto:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="geometria_conductor" class="form-control">
           </div>
           <div class="col-md-4">
             <label>Diámetro interior del conducto, Dch:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="diametro_interior_conducto" class="form-control">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Diámetro equivalente, Deq:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="diametro_equivalente" class="form-control">
           </div>
           <div class="col-md-4">
             <label>Largo transversal, L1:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="L1" class="form-control">
           </div>
           <div class="col-md-4">
             <label>Ancho transversal, L2:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="L2" class="form-control">
           </div>
         </div>
-
+<!-- aqui -->
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Número de puertos:</label>
-            <input type="text" class="form-control">
+            <input type="text" name="no_puertos" class="form-control">
           </div>
           <div class="col-md-4">
             <label>Distancia en A:</label>
@@ -194,7 +166,6 @@
             <input type="text" class="form-control">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-4">
             <label>Número de diámetros en A:</label>
@@ -209,7 +180,6 @@
             <input type="text" class="form-control">
           </div>
         </div>
-
         <div class="row mb-3">
           <div class="col-md-6">
             <label>Número de puntos seleccionados para medición de gases:</label>
@@ -217,9 +187,6 @@
           </div>
         </div>
       </form>
-
-
-
 </div>
 
 <div id="div3">
@@ -264,8 +231,6 @@
   </tbody>
 </table>
 
-
-
 </div>
 
 <div id="div4">
@@ -294,23 +259,16 @@
 
 <br><br><br> <hr>
 
+<button id="btnGuardar" class="btn btn-success">Guardar</button>
 
 </div>
 
 </div>
-
-
-
 </div>
 
-
-
-
-
-<!--******************************CONTENT-->
-                </div> <!-- END Page Content -->
+                </div>
             </main>
-            <!-- END Main Container -->
+
             <script type="text/javascript">
 				
             	$(document).ready(function(){
@@ -322,34 +280,160 @@
                 });
 
                 
-                $(document).ready(function() {
-    $('#normaSelect').change(function() {
-        const norma = $(this).val();
-        if (norma) {
-            $.post('NormaController/get_formulario', { norma }, function(data) {
-                $('#formularioNorma').html(data);
-            });
-        } else {
-            $('#formularioNorma').empty();
+                $(document).ready(function(){
+
+    $('#normaSelect').select2({
+        placeholder: "Selecciona una opción",
+        width: '100%'
+    });
+
+    let tabla = `
+        <form action="">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr class="text-center">
+                            <th>N°</th>
+                            <th>Nox(ppmv)</th>
+                            <th>CO (ppmv)</th>
+                            <th>O2(%)</th>
+                            <th>CO<sub>2</sub> %</th>
+                            <th>Temp, En el Conducto C°</th>
+                        </tr>
+                    </thead>
+                    <tbody id="CamposRegistros">
+                        
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="5"></td>
+                            <td colspan="1">
+                                <div class="d-grid">
+                                    <input type="submit" value="Agregar" class="btn btn-primary">
+                                </div>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </form>
+    `;
+
+
+    let tabla2 = `
+        <form action="">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr class="text-center">
+                            <th>N°</th>
+                            <th>CO (ppmv)</th>
+                            <th>O2(%)</th>
+                            <th>CO<sub>2</sub> %</th>
+                            <th>Temp, En el Conducto C°</th>
+                        </tr>
+                    </thead>
+                    <tbody id="CamposRegistros2">
+                        
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="4"></td>
+                            <td colspan="1">
+                                <div class="d-grid">
+                                    <input type="submit" value="Agregar" class="btn btn-primary">
+                                </div>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </form>
+    `;
+
+    function filas2(){
+        $("#CamposRegistros2").empty();
+
+        for(let i=0;i<60; i++){
+            let campo = ` 
+                <tr> 
+                    <td>${i+1}</td> 
+                    <td><input type="number" class="form-control" name="CO"/></td> 
+                    <td><input type="number" class="form-control" name="O2" step="0.01"/></td> 
+                    <td><input type="number" class="form-control" name="CO2" step="0.01"/></td> 
+                    <td><input type="number" class="form-control" name="Temp" step="0.1"/></td> 
+                </tr>`; 
+
+            $("#CamposRegistros2").append(campo);
         }
-    });
-
-    $(document).on('submit', '#formNorma', function(e) {
-        e.preventDefault();
-        const datos = $(this).serialize();
-        $.post('NormaController/guardar_dato', datos, function(res) {
-            alert('Dato guardado');
-            cargarTabla();
-        });
-    });
-
-    function cargarTabla() {
-        $.get('NormaController/obtener_tabla', function(html) {
-            $('#tablaNorma').html(html);
-        });
     }
 
-    cargarTabla(); // inicial
+    function filas(){
+        $("#CamposRegistros").empty();
+
+        for(let i=0;i<60; i++){
+            let campo = ` 
+                <tr> 
+                    <td>${i+1}</td> 
+                    <td><input type="number" class="form-control" name="Nox" step="0.01"/></td> 
+                    <td><input type="number" class="form-control" name="CO"/></td> 
+                    <td><input type="number" class="form-control" name="O2" step="0.01"/></td> 
+                    <td><input type="number" class="form-control" name="CO2" step="0.01"/></td> 
+                    <td><input type="number" class="form-control" name="Temp" step="0.1"/></td> 
+                </tr>`; 
+
+            $("#CamposRegistros").append(campo);
+        }
+    }
+
+    $("#normaSelect").change(function(){
+        let opcion = $("#normaSelect").val();
+        if(opcion == "085MG" || opcion == "085ML"){
+            $('#tablas').html(tabla);
+            filas();
+        }else if(opcion == "085G" || opcion == "085L"){
+            $('#tablas').html(tabla2);
+            filas2();
+        }
+    });
+    
 });
-            
+
+document.getElementById("btnGuardar").addEventListener("click", function () {
+  const datos = {
+    numero_informe: document.querySelector('[name="numero_informe"]').value,
+    orden_servicio: document.querySelector('[name="orden_servicio"]').value,
+    fecha_evaluacion: document.querySelector('[name="fecha_evaluacion"]').value,
+    recepcion: document.querySelector('[name="recepcion"]').value,
+    fecha_informe: document.querySelector('[name="fecha_informe"]').value,
+    razon_social: document.querySelector('[name="razon_social"]').value,
+    calle: document.querySelector('[name="calle"]').value,
+    colonia: document.querySelector('[name="colonia"]').value,
+    alcaldia: document.querySelector('[name="alcaldia"]').value,
+    estado: document.querySelector('[name="estado"]').value,
+    cp: document.querySelector('[name="cp"]').value,
+    responsable: document.querySelector('[name="responsable"]').value,
+    cargo: document.querySelector('[name="cargo"]').value,
+    telefono: document.querySelector('[name="telefono"]').value,
+    equipo_evaluado: document.querySelector('[name="equipo_evaluado"]').value,
+    marca: document.querySelector('[name="marca"]').value,
+    combustible: document.querySelector('[name="combustible"]').value,
+    capacidad_termica: document.querySelector('[name="capacidad_termica"]').value,
+    altura: document.querySelector('[name="altura"]').value,
+    presion: document.querySelector('[name="presion"]').value,
+    anio: document.querySelector('[name="anio"]').value,
+    presion_barometrica: document.querySelector('[name="presion_barometrica"]').value,
+    geometria_conductor: document.querySelector('[name="geometria_conductor"]').value,
+    diametro_interior_conducto: document.querySelector('[name="diametro_interior_conducto"]').value,
+    diametro_equivalente: document.querySelector('[name="diametro_equivalente"]').value,
+    L1: document.querySelector('[name="L1"]').value,
+    L2: document.querySelector('[name="L2"]').value,
+    no_puertos: document.querySelector('[name="no_puertos"]').value,
+  };
+  console.log(datos);
+  
+
+
+});
+
             </script>
